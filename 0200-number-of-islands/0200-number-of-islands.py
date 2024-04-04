6f883309e384a7e0c593aeee10c663c38b7df447
Mulugeta Hailegnaw Belay <91776713+mulugeta-89@users.noreply.github.com>
2024-04-04 19:03:25 +0300

    Time: 263 ms (39.34%), Space: 18.8 MB (83.22%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,28 @@
-def explore(grid, r,c, visited):
-    rowInbounds = r >= 0 and r < len(grid)
-    colInbounds = c >= 0 and c < len(grid[0])
-    if not rowInbounds or  not colInbounds or grid[r][c] == "0":
-        return False
-    position = str(r) + "," + str(c)
-    if position in visited:
-        return False
-    visited.add(position)
-    explore(grid, r-1, c, visited)
-    explore(grid, r+1, c, visited)
-    explore(grid, r, c-1, visited)
-    explore(grid, r, c+1, visited)
-    return True
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = set()
+        row, col = len(grid), len(grid[0])
+        visited = [[False for i in range(col)] for j in range(row)]
         count = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if explore(grid, r, c, visited):
-                    count += 1
+        def inbound(r, c):
+            return 0 <= r < row and 0 <= c < col 
+        
+        directions = [(-1,0), (1,0), (0,1), (0,-1)]
+
+        def dfs(grid, r, c, visited):
+            visited[r][c] = True
+            for x, y in directions:
+                new_r, new_c = r + x, c + y
+                if inbound(new_r, new_c) and not visited[new_r][new_c] and grid[new_r][new_c] == "1":
+                    dfs(grid, new_r, new_c, visited)
+            return True
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == "1" and not visited[r][c]:
+                    if dfs(grid, r, c, visited):
+                        count += 1
         return count
+
+
+
+
         
